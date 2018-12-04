238803f83de5f35b1bbf5c8843139cb573aa4492
trdtnguyen <trauconnguyen@gmail.com>
2018-12-04 16:11:18 +0900

    add .sh files for perf

---
+++ ycsb-mongo/start_perf_record.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+source const.sh
+#Run perf record for a benchmark
+
+out_file=perf.data
+
+#Change this to your desired program e.g, mongod, mysqld
+p_name=mongod
+
+pid=$(ps -opid= -C $p_name)
+
+
+if [ -n $1 ]; then
+	out_file=$1
+	echo "output file is $1"
+fi
+
+echo "Run perf for program named $p_name with pid=$pid ..."
+sudo perf record -p $pid -a -F 1000 --group -o $out_file
+
+echo "Perf finished!"
